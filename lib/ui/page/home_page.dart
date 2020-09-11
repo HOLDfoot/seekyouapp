37679a0beb7f0bdeb8cf1a14b60f6491315cf7ca
ZhuMingren <mingren@cecelive.com>
2020-09-11 15:06:27 +0800

    Add嵌入flutter easyrefresh

--- lib/ui/page/home_page.dart
+++ lib/ui/page/home_page.dart
@@ -1,87 +1,192 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_easyrefresh/easy_refresh.dart';
+import 'package:flutter_easyrefresh/ball_pulse_header.dart';
+import 'package:flutter_easyrefresh/ball_pulse_footer.dart';
 
-/// 首页
+/// 会员购买记录
 class HomePage extends StatefulWidget {
-  HomePage({Key key, this.title = "首页"}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  _HomePageState createState() => _HomePageState();
+  HomePageState createState() {
+    return HomePageState();
+  }
 }
 
-class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomePageState extends State<HomePage> {
+  List<UserInfo> orderList;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: EasyRefresh(
+          emptyWidget: getEmptyWidget(),
+          firstRefresh: true,
+          header: BallPulseHeader(
+            enableHapticFeedback: false,
+          ),
+          footer: BallPulseFooter(
+            enableHapticFeedback: false,
+          ),
+          onRefresh: onRefresh,
+          onLoad: noMore ? null : onLoad,
+          child: ListView.builder(
+            padding: EdgeInsets.only(top: adapt(4)),
+            shrinkWrap: true,
+            itemCount: getChildCount(),
+            physics: NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              return getChildWidget(index);
+            },
+          )
+/*
+          child: ListView.builder(
+            padding: EdgeInsets.only(top: adapt(4)),
+            shrinkWrap: true,
+            itemCount: getChildCount(),
+            physics: NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              return getChildWidget(index);
+            },
+          )
+*/
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  /// 后端默认10
+  int pageSize = 10;
+
+  /// getData负责+1
+  int page = 0;
+
+  /// 默认没有更多, 只有当刷新成功了才有更多
+  bool noMore = true;
+
+  /// 网络失败则返回null, 成功则有数据
+  Future<List<UserInfo>> getData() async {
+    UserInfo item = UserInfo(name: "小明", photo: CONST_PIC);
+    List<UserInfo> userInfoList = [];
+    for (int i = 0; i < pageSize; i++) {
+      userInfoList.add(item);
+    }
+    print(DateTime.now());
+    await Future.delayed(Duration(seconds: 3));
+    print(DateTime.now());
+    return userInfoList;
+  }
+
+  /// 刷新页面
+  /// 处理了: 网络异常, 第一页的分页, 还有更多的情况, 第一次加载展示加载的UI
+  Future<void> onRefresh() async {
+    print("onRefresh 1");
+    // 恢复初始值
+    page = 1;
+    List<UserInfo> orderData = await getData();
+    // 只要网络请求回来, 就已经完成了第一次加载
+    if (showedLoading) {
+      showedLoading = false;
+    }
+    if (mounted) {
+      setState(() {
+        // 如果不是网络异常, 则不为空, 更新列表, 如果是网络异常则为空, 不更新
+        if (orderData != null) {
+          orderList = orderData;
+          noMore = false;
+        }
+      });
+    }
+  }
+
+  /// 加载更多
+  /// 处理了: 没有更多的情况, 网络失败的情况, 分页的情况
+  Future<void> onLoad() async {
+    print("onLoad 1");
+
+    page++;
+    List<UserInfo> orderData = await getData();
+    if (mounted) {
+      setState(() {
+        if (orderData == null) {
+          /// 网络失败
+          page--;
+        } else if (orderData.length == 0) {
+          noMore = true;
+          orderList.addAll(orderData);
+          page--;
+        } else {
+          noMore = false;
+          orderList.addAll(orderData);
+        }
+      });
+    }
+  }
+
+  /// 是否已经展示过loading
+  bool showedLoading = true;
+
+  /// 空视图一个是第一次加载, 一个是网络错误orderList==null, 一个是数据是空数组orderList==[]
+  /// 如果@return是null, 则不显示null
+  Widget getEmptyWidget() {
+    if (getChildCount() > 0) {
+      return null;
+    }
+    if (showedLoading) {
+      // 第一次加载
+      return SearchResultEmptyWidget(
+        title: "正在努力加载中",
+      );
+    } else if (orderList == null) {
+      // 不是第一次加载情况下仍然为空, 则肯定是网络错误
+      return SearchResultEmptyWidget(
+        title: "网络错误",
+      );
+    } else {
+      // orderList==[]
+      // 空界面的情况
+      return SearchResultEmptyWidget();
+    }
+  }
+
+  /// 获取子item的数值
+  int getChildCount() {
+    int count = (orderList ?? List()).length;
+    return count;
+  }
+
+  /// 获取item Widget
+  Widget getChildWidget(int index) {
+    return Card(
+      color: Colors.green,
+      margin: EdgeInsets.all(10),
+      child: Text(orderList[index].name + " $index"),
+    );
+  }
+
+  double adapt(int dp) {
+    return dp * 1.0;
+  }
 }
+
+class UserInfo {
+  String name;
+  String email;
+  String photo;
+  String introduce;
+
+  UserInfo({this.name, this.email, this.photo, this.introduce});
+
+}
+
+class SearchResultEmptyWidget extends StatelessWidget {
+  final String title;
+
+  SearchResultEmptyWidget({Key key, this.title = "没有数据"}) :super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(this.title);
+  }
+}
+
+const String CONST_PIC = "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=375015144,848773525&fm=26&gp=0.jpg";
